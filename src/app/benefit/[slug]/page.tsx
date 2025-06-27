39d3054e1317dd15b0c9e63b468d6491373ab17b
難波隼人 <hayatonanba@gmail.com>
2025-06-27 13:45:40 +0900

    init

---
+++ src/app/benefit/[slug]/page.tsx
@@ -0,0 +1,14 @@
+interface BenefitProps {
+  params: {
+    slug: string;
+  }
+}
+
+export default function Benefit({ params }: BenefitProps) {
+
+    return (
+        <div>
+         <h2>mock page</h2>
+        </div>
+    );
+}

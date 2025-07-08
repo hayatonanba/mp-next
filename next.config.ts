172d5fac8d3b718766f2d480b0afc83c19507547
難波隼人 <hayatonanba@gmail.com>
2025-07-09 00:02:42 +0900

    chore: ogp-textfix-etc

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {domains: ['images.pexels.com'],}
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
